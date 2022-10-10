86283c6227d6240fce7abd1790cdaef1972b6dd7
Tam Mach <tam.mach@cilium.io>
2022-10-10 14:32:08 +1100

    gateway-api: Add reconciliation step for HTTPRoute
    For HTTPRoute resources, the reconciliation should start if any of below
    event happens:
    
    - Changes in HTTPRoute itself
    - Changes in related backend services
    - Changes in parent Gateway spec (e.g. allowedRoutes)
    
    The current reconciliation loop is trying its best to make sure that HTTPRoute
    is attachable to Gateway. If all validations are passed, then the Accepted
    condition will be updated to True, which signals the reconciliation loop for
    parent Gateway resources.
    
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

---
+++ operator/pkg/gateway-api/helpers.go
@@ -0,0 +1,101 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package gateway_api
+
+import (
+	"context"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	gatewayv1beta1 "sigs.k8s.io/gateway-api/apis/v1beta1"
+
+	"github.com/cilium/cilium/operator/pkg/model"
+)
+
+const (
+	kindGateway   = "Gateway"
+	kindHTTPRoute = "HTTPRoute"
+	kindService   = "Service"
+	kindSecret    = "Secret"
+)
+
+func IsService(be gatewayv1beta1.BackendObjectReference) bool {
+	return (be.Kind == nil || *be.Kind == kindService) && (be.Group == nil || *be.Group == corev1.GroupName)
+}
+
+func IsSecret(secret gatewayv1beta1.SecretObjectReference) bool {
+	return (secret.Kind == nil || *secret.Kind == kindSecret) && (secret.Group == nil || *secret.Group == corev1.GroupName)
+}
+
+func namespaceDerefOr(namespace *gatewayv1beta1.Namespace, defaultNamespace string) string {
+	if namespace != nil && *namespace != "" {
+		return string(*namespace)
+	}
+	return defaultNamespace
+}
+
+// isAllowed returns true if the provided HTTPRoute is allowed to attach to given gateway
+func isAllowed(ctx context.Context, c client.Client, gw *gatewayv1beta1.Gateway, hr *gatewayv1beta1.HTTPRoute) bool {
+	for _, listener := range gw.Spec.Listeners {
+		kindAllowed, nsAllowed := listener.AllowedRoutes == nil, listener.AllowedRoutes == nil
+		if listener.AllowedRoutes != nil {
+			kindAllowed = listener.AllowedRoutes.Kinds == nil
+			for _, kind := range listener.AllowedRoutes.Kinds {
+				if (kind.Group == nil || string(*kind.Group) == gatewayv1beta1.GroupName) &&
+					kind.Kind == kindHTTPRoute {
+					kindAllowed = true
+					break
+				}
+			}
+
+			nsAllowed = listener.AllowedRoutes.Namespaces == nil
+			if listener.AllowedRoutes.Namespaces != nil {
+				switch *listener.AllowedRoutes.Namespaces.From {
+				case gatewayv1beta1.NamespacesFromAll:
+					nsAllowed = true
+				case gatewayv1beta1.NamespacesFromSame:
+					nsAllowed = hr.GetNamespace() == gw.GetNamespace()
+				case gatewayv1beta1.NamespacesFromSelector:
+					nsList := &corev1.NamespaceList{}
+					selector, _ := metav1.LabelSelectorAsSelector(listener.AllowedRoutes.Namespaces.Selector)
+					if err := c.List(ctx, nsList, client.MatchingLabelsSelector{Selector: selector}); err != nil {
+						log.WithError(err).Error("Unable to list namespaces")
+						return false
+					}
+
+					for _, ns := range nsList.Items {
+						if ns.Name == hr.GetNamespace() {
+							nsAllowed = true
+							break
+						}
+					}
+				}
+			}
+		}
+
+		if !kindAllowed || !nsAllowed {
+			return false
+		}
+	}
+
+	return true
+}
+
+func computeHosts(gw *gatewayv1beta1.Gateway, hr *gatewayv1beta1.HTTPRoute) []string {
+	hosts := make([]string, 0, len(hr.Spec.Hostnames))
+	for _, listener := range gw.Spec.Listeners {
+		hosts = append(hosts, model.ComputeHosts(toStringSlice(hr.Spec.Hostnames), (*string)(listener.Hostname))...)
+	}
+
+	return hosts
+}
+
+func toStringSlice(s []gatewayv1beta1.Hostname) []string {
+	res := make([]string, 0, len(s))
+	for _, h := range s {
+		res = append(res, string(h))
+	}
+	return res
+}
